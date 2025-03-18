a1a34a7a1bd46a5c2307cd2feb2888808de227a8
LautaroDiana <lautarodiana@gmail.com>
2025-03-18 16:14:13 -0300

    starting to work on front

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,14 +1,18 @@
 import express from "express";
 import dotenv from "dotenv";
 import authRouter from "./routes/auth.route.js";
+import messageRouter from "./routes/message.route.js";
 import { connectDb } from "./lib/db.js";
+import cookieParser from "cookie-parser";
 
 const app = express();
 
 dotenv.config();
 
 app.use(express.json());
+app.use(cookieParser());
 app.use("/api/auth", authRouter);
+app.use("/api/message", messageRouter);
 
 const PORT = process.env.PORT || 5001;
 
