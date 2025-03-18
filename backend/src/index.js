97025a9ebb3cb3b48f96f4a05137f52f59b76a00
LautaroDiana <lautarodiana@gmail.com>
2025-03-18 17:42:58 -0300

    React router implemented

--- backend/src/index.js
+++ backend/src/index.js
@@ -4,6 +4,7 @@ import authRouter from "./routes/auth.route.js";
 import messageRouter from "./routes/message.route.js";
 import { connectDb } from "./lib/db.js";
 import cookieParser from "cookie-parser";
+import cors from "cors";
 
 const app = express();
 
@@ -11,6 +12,12 @@ dotenv.config();
 
 app.use(express.json());
 app.use(cookieParser());
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+    credentials: true,
+  })
+);
 app.use("/api/auth", authRouter);
 app.use("/api/message", messageRouter);
 
