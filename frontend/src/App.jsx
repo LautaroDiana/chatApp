20c08f5bf0d7fd52dc7c289327e57e383662e04b
LautaroDiana <lautarodiana@gmail.com>
2025-04-08 12:36:51 -0300

    Settings page and custom theme implementation

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -6,12 +6,13 @@ import { LoginPage } from "./pages/LoginPage";
 import { SettingsPage } from "./pages/SettingsPage";
 import { ProfilePage } from "./pages/ProfilePage";
 import { useAuthStore } from "./store/useAuthStore";
+import { useThemeStore } from "./store/useThemeStore";
 import { useEffect } from "react";
 import { Loader } from "lucide-react";
 import { Toaster } from "react-hot-toast";
 export default function App() {
   const { authUser, checkAuth, isCheckingAuth } = useAuthStore();
-
+  const { theme } = useThemeStore();
   useEffect(() => {
     checkAuth();
   }, [checkAuth]);
@@ -26,7 +27,7 @@ export default function App() {
     );
 
   return (
-    <div>
+    <div data-theme={theme}>
       <Navbar />
 
       <Routes>
