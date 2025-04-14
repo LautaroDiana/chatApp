a6df04d468028b208285c8b80b77a65a1136299a
LautaroDiana <lautarodiana@gmail.com>
2025-04-14 17:56:04 -0300

    Working on chat page

--- frontend/src/components/ChatContainer.jsx
+++ frontend/src/components/ChatContainer.jsx
@@ -1,3 +1,33 @@
+import { useChatStore } from "../store/useChatStore";
+import { useEffect } from "react";
+import ChatHeader from "../components/ChatHeader";
+import MessageInput from "../components/MessageInput";
+import MessageSkeleton from "./skeletons/MessageSkeleton";
+
 export const ChatContainer = () => {
-  return <div>ChatContainer</div>;
+  const { messages, getMessages, isMessagesLoading, selectedUser } =
+    useChatStore();
+
+  useEffect(() => {
+    getMessages(selectedUser._id);
+  }, [selectedUser._id, getMessages]);
+
+  if (isMessagesLoading) {
+    return (
+      <div className="flex-1 flex flex-col overflow-auto">
+        <ChatHeader />
+        <MessageSkeleton />
+        <MessageInput />
+      </div>
+    );
+  }
+
+  return (
+    <div className="flex-1 flex flex-col overflow-auto">
+      <ChatHeader />
+      <p>messages...</p>
+
+      <MessageInput />
+    </div>
+  );
 };
